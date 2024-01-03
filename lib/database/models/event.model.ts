d31ef2e0bc43511e4ae052cfff40b954bafa9546
karimscoding <rejaulkarim66666@gmail.com>
2024-01-04 00:27:41 +0800

    implement user auth

---
+++ lib/database/models/event.model.ts
@@ -0,0 +1,63 @@
+import { Schema, model, models } from "mongoose";
+
+export interface IEvent extends Document {
+  _id: string;
+  title: string;
+  description?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price?: string;
+  isFree: boolean;
+  url?: string;
+  category?: { _id: string; name: string };
+  organizer?: { _id: string; firstName: string; lastName: string };
+}
+
+const eventSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  location: {
+    type: String,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  startDateTime: {
+    type: Date,
+    default: Date.now,
+  },
+  endDateTime: {
+    type: Date,
+    default: Date.now,
+  },
+  price: {
+    type: String,
+  },
+  isFree: {
+    type: Boolean,
+    default: false,
+  },
+  url: { type: String },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  organizer: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+const Event = models.user || model("event", eventSchema);
+
+export default Event;
---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,38 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  photoUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = models.user || model("user", userSchema);
+
+export default User;
