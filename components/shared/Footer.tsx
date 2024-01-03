541266d917538dbdffa44df7da8986076de8ae78
karimscoding <rejaulkarim66666@gmail.com>
2024-01-03 14:18:39 +0800

    added hero section

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+import { Icons } from "./Icons";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 text-center sm:flex-row">
+        <Link href="/">
+          <Icons.logo className="w-10 h-10" />
+        </Link>
+
+        <p>2024 Eventliee. All Rignt reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
